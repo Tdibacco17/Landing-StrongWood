59b39c89401032b5fd2381f3cb709fcba8f4d93c
BrayanCode11 <bangarita@unal.edu.co>
2025-08-28 20:38:24 -0500

    Finalizacion del footer

--- src/layouts/footers/FooterOne.tsx
+++ src/layouts/footers/FooterOne.tsx
@@ -32,87 +32,13 @@ const FooterOne = () => {
             <div className="footer-main bg-color-1 p-relative">
                <div className="footer-shape-1" style={{ backgroundImage: `url(/assets/imgs/footer/shape-f-1.png)` }}></div>
                <div className="footer-shape-2" style={{ backgroundImage: `url(/assets/imgs/footer/shape-f-2.png)` }}></div>
-               <div className="footer-top section-space-medium">
-                  <div className="small-container">
-                     <div className="row g-4">
-                        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-6">
-                           <div className="footer-widget-1">
-                              <figure className="image">
-                                 <Image src={logo_1} alt="" />
-                              </figure>
-                              <p className="mt-40 mb-30">
-                                 Phasellus ultricies aliquam volutpat
-                                 ullamcorper laoreet neque, a lacinia
-                                 curabitur lacinia mollis
-                              </p>
-                              <div className="working-hours">
-                                 <h6 className="text-white mb-20">Working Hours:</h6>
-                                 <ul className="text-white">
-                                    <li>Mon - Sat: <span className="fw-lighter">10.00AM - 4.00PM</span></li>
-                                    <li>Sunday: <span className="fw-lighter">Close</span></li>
-                                 </ul>
-                              </div>
-                           </div>
-                        </div>
-                        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-6">
-                           <div className="footer-widget-2 pl-50">
-                              <h4 className="mb-30 footer-title">Quick Links</h4>
-                              <ul className="service-list">
-                                 <li><Link href="/about">About Us</Link></li>
-                                 <li><Link href="/team">Our Team</Link></li>
-                                 <li><Link href="/services">Our Services</Link></li>
-                                 <li><Link href="/blog-grid">Latest Blog</Link></li>
-                                 <li><Link href="/contact">Contact Us</Link></li>
-                              </ul>
-                           </div>
-                        </div>
-                        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-6">
-                           <div className="footer-widget-2 pl-50">
-                              <h4 className="mb-30 footer-title">Our Services</h4>
-                              <ul className="service-list">
-                                 <li><Link href="/service-details">Damage Roofing</Link></li>
-                                 <li><Link href="/service-details">Roof Repair</Link></li>
-                                 <li><Link href="/service-details">Roof Installation</Link></li>
-                                 <li><Link href="/service-details">Roof Animation</Link></li>
-                                 <li><Link href="/service-details">Modified Roofing</Link></li>
-                              </ul>
-                           </div>
-                        </div>
-                        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-6">
-                           <div className="footer-widget-4 pr-30">
-                              <h4 className="mb-20 footer-title mb-30">Our Gallery</h4>
-                              <div className="footer-gallery p-relative">
-                                 {gallery_data.map((gallery, i) => (
-                                    <a
-                                       key={i}
-                                       onClick={() => handleOpen(i)}
-                                       className="popup-image"
-                                       style={{ padding: 0, border: "none", background: "transparent", cursor: "pointer" }}
-                                    >
-                                       <Image src={gallery} alt={`Gallery image ${i + 1}`} />
-                                    </a>
-                                 ))}
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
+               
                <div className="small-container">
                   <div className="footer-bottom pt-30 pb-30">
-                     <div className="left-area p-relative">
-                        <span>© All Copyright 2025 by <Link href="#">Gramen</Link></span>
+                     <div className="center-area p-relative">
+                        <span>© 2025 StrongWood. Todos los derechos reservados. Desarrollado por <Link href="https://www.mrostagnol.com/google/ads">Rostagnol Media</Link></span>
                      </div>
-                     <div className="footer-socials p-relative">
-                        <span><Link href="#"><i className="fab fa-facebook-f"></i></Link></span>
-                        <span><Link href="#"><i className="fab fa-twitter"></i></Link></span>
-                        <span><Link href="#"><i className="fab fa-linkedin-in"></i></Link></span>
-                        <span><Link href="#"><i className="fab fa-youtube"></i></Link></span>
-                     </div>
-                     <div className="right-area p-relative">
-                        <span><Link href="#">Terms & Condition</Link></span>
-                        <span><Link href="#">Privacy Policy</Link></span>
-                     </div>
+               
                   </div>
                </div>
             </div>
