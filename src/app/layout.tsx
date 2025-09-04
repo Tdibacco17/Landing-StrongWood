a7cb4ba6c86e30d70774dbc86fa78533f7409829
Tdibacco17 <tomasdibacco@gmail.com>
2025-09-04 12:47:50 -0300

    tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,26 +41,28 @@ export default function RootLayout({
       {/* <link rel="icon" href="/favicon.png" sizes="any" /> */}
       {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" /> */}
       {/* </head> */}
-      <Script id="gtm" strategy="afterInteractive">
-        {`
+      <head>
+        <Script id="gtm" strategy="afterInteractive">
+          {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-      </Script>
+          })(window,document,'script','dataLayer','GTM-NP65ZB78');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${roboto.variable}`} suppressHydrationWarning={true}>
         {/* Noscript de GTM: debe ir inmediatamente dentro de <body> */}
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-NP65ZB78`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
-        
+
         <Providers>
           {children}
         </Providers>
