2e64e68956d86439c265769a0f4c57b8b3a3434d
Tomás Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2025-09-04 12:43:21 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 // import "../styles/index.css";
 import { Inter, Roboto } from "next/font/google";
 import 'react-toastify/dist/ReactToastify.css';
+import Script from "next/script";
 import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -40,9 +41,27 @@ export default function RootLayout({
       {/* <link rel="icon" href="/favicon.png" sizes="any" /> */}
       {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" /> */}
       {/* </head> */}
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body className={`${inter.variable} ${roboto.variable}`} suppressHydrationWarning={true}>
+        {/* Noscript de GTM: debe ir inmediatamente dentro de <body> */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
         <Providers>
-          
           {children}
         </Providers>
       </body>
